46260ae637c4eaf1faa7a25b7fa72b57141f7f12
Idris Dose <idrisdev@gmail.com>
2021-11-03 18:35:34 +1100

    feat (DrawText): Added ChangeText to change text instead of hide/show.

--- client/main.lua
+++ client/main.lua
@@ -26,6 +26,19 @@ local function DrawText(text, position)
     })
 end
 
+
+local function ChangeText(text,position)
+    if (not type(position) == "string") then position = "left" end
+
+    SendNUIMessage({
+        action = 'CHANGE_TEXT',
+        data = {
+            text = text,
+            position = position
+        }
+    })
+end
+
 local function KeyPressed()
     if not textIsVisible then return end
 
@@ -42,6 +55,10 @@ RegisterNetEvent('qb-drawtext:client:DrawText', function(text, position)
     DrawText(text, position)
 end)
 
+RegisterNetEvent('qb-drawtext:client:ChangeText', function(text, position)
+    ChangeText(text, position)
+end)
+
 RegisterNetEvent('qb-drawtext:client:HideText', function()
     HideText()
 end)
@@ -51,5 +68,6 @@ RegisterNetEvent('qb-drawtext:client:KeyPressed', function()
 end)
 
 exports('DrawText', DrawText)
+exports('ChangeText', ChangeText)
 exports('HideText', HideText)
 exports('KeyPressed', KeyPressed)
--- html/script.js
+++ html/script.js
@@ -1,13 +1,8 @@
 let direction = null;
-let key = "";
 
 const drawText = (textData) => {
     const text = document.getElementById("text");
     document.getElementById("container").style.display = "block";
-
-    if (textData.actionKey) {
-        actionKey = key;
-    }
 
     switch (textData.position) {
         case "left":
@@ -33,6 +28,41 @@ const drawText = (textData) => {
         text.classList.add("show");
     }, 100);
 };
+
+const changeText = (textData) => {
+    const text = document.getElementById("text");
+    text.classList.remove("show");
+    text.classList.remove("left");
+    text.classList.remove("right");
+    text.classList.remove("top");
+    text.classList.remove("bottom");
+    text.classList.remove("hide");
+    text.classList.remove("pressed");
+
+    switch (textData.position) {
+        case "left":
+            text.classList.add("left");
+            direction = "left";
+            break;
+        case "top":
+            text.classList.add("top");
+            direction = "top";
+            break;
+        case "right":
+            text.classList.add("right");
+            direction = "right";
+            break;
+        default:
+            text.classList.add("left");
+            direction = "left";
+            break;
+    }
+    text.innerHTML = textData.text;
+
+    setTimeout(() => {
+        text.classList.add("show");
+    }, 100);
+}
 
 const hideText = () => {
     const text = document.getElementById("text");
@@ -62,6 +92,8 @@ window.addEventListener("message", (event) => {
     switch (action) {
         case "DRAW_TEXT":
             return drawText(textData);
+        case "CHANGE_TEXT":
+            return changeText(textData);
         case "HIDE_TEXT":
             return hideText();
         case "KEY_PRESSED":
